1af156aed1a3481b7fe5131f6db232fdb0b4550b
alfesen <89510380+alfesen@users.noreply.github.com>
2023-03-16 11:51:28 +0100

    Create hook for fetching data from API

---
+++ src/hooks/useFetchData.tsx
@@ -0,0 +1,30 @@
+import { useState, useCallback } from 'react'
+
+const useFetchData = () => {
+  const [error, setError] = useState<null | string>(null)
+  const [loading, setLoading] = useState(false)
+
+  const sendRequest = useCallback(async (url: string) => {
+    setLoading(true)
+    let responseData: any
+    try {
+      const response = await fetch(url)
+      responseData = await response.json()
+      if (!response.ok) {
+        console.log()
+        const errorText = 'Failed to fetch data'
+        setError(errorText)
+        throw new Error(errorText)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+    return responseData
+  }, [])
+  
+  return { sendRequest, loading, error }
+}
+
+export default useFetchData
