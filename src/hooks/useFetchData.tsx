056881653cef81751fb16f37f78fc79043e6b1fa
alfesen <89510380+alfesen@users.noreply.github.com>
2023-03-18 16:00:37 +0100

    Edit useFetchData hook for accepting different methods

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,25 +1,53 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
 
 const useFetchData = () => {
   const [error, setError] = useState<null | string>(null)
   const [loading, setLoading] = useState(false)
 
-  const sendRequest = useCallback(async (url: string) => {
-    setLoading(true)
-    let responseData: any
-    try {
-      const response = await fetch(url)
-      responseData = await response.json()
-    } catch (err) {
-      const errorText = 'Failed to fetch data'
-      setError(errorText)
-      console.log(err)
-    }
+  const activeHttpRequests = useRef<any>([])
 
-    setLoading(false)
-    return responseData
+  const sendRequest = useCallback(
+    async (
+      url: string,
+      method: string,
+      body: string | null = null,
+      headers = {}
+    ) => {
+      setLoading(true)
+      const httpAbort = new AbortController()
+      activeHttpRequests.current.push(httpAbort)
+
+      let responseData: any
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbort.signal,
+        })
+        responseData = await response.json()
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (req: AbortController) => req !== httpAbort
+        )
+      } catch (err) {
+        const errorText = 'Failed to fetch data'
+        setError(errorText)
+        console.log(err)
+      }
+
+      setLoading(false)
+      return responseData
+    },
+    []
+  )
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((ctrl: AbortController) =>
+        ctrl.abort()
+      )
+    }
   }, [])
-  
   return { sendRequest, loading, error }
 }
 
